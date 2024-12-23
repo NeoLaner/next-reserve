760163e3604ccb338754827d4578ccea0d9a9f59
NeoLaner <142805740+NeoLaner@users.noreply.github.com>
2024-12-23 11:30:50 +0330

    Add files via upload
    init

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    try {
+     
+      return Response.json("hi");
+   
+    } catch (error) {
+     
+    }
+  }
+  
